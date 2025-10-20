8bd418e07c276d6b8c0c7bcdc3c0b6590ba2c75a
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-10-20 19:25:28 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main (String[] args){
+        String ad = "Cuma";
+        String soyad = "Doğan";
+        String ogrNo = "250542003";
+        String bolum = "Yazilim Muh.";
+        System . out . println ("=================== ") ;
+        System . out . println ("OGRENCI BILGILERI ") ;
+        System . out . println ("=================== ") ;
+        System . out . println ("Ad : " + ad ) ;
+        System . out . println ("Soyad : " + soyad ) ;
+        System . out . println ("No : " + ogrNo ) ;
+        System . out . println ("Bolum : " + bolum ) ;
+        System . out . println ("=================== ") ;
+
+    }
+}
