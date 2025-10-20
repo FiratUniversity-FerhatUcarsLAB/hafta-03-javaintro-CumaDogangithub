a928c1bd4f61cd01b7300eb1e27a5fd8b822f269
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-10-20 13:48:42 +0300

    Update UcMesaj.java

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -4,18 +4,6 @@ public class UcMesaj {
         System.out.println("Bilgisayar Bilimlerine Hoşgeldin");
         System.out.println("Programlama Eglencelidir");
         System.out.println("");
-
-        String java="Java'ya Hoş Geldiniz";
-        System.out.println(java);
-        System.out.println(java);
-        System.out.println(java);
-        System.out.println(java);
-        System.out.println(java);
-
-        System.out.println("     J A V V A     ");
-        System.out.println("   J A A V V A A   ");
-        System.out.println("J J AAAAA V V AAAAA");
-        System.out.println("   J A A V V A A   ");
-
     }
 }
+
