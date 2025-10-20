570ae4ac85392e31edc2a0b10a4d877fefee9a90
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-10-20 13:48:28 +0300

    Add BesKereTekrar class to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        String java="Java'ya Hoş Geldiniz";
+        System.out.println(java);
+        System.out.println(java);
+        System.out.println(java);
+        System.out.println(java);
+        System.out.println(java);
+    }
+}
