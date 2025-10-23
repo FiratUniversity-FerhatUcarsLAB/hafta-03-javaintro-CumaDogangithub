b915c26222ce2eae40b6fa00c837f808c90f0412
Cuma doğan <155235759+CumaDogangithub@users.noreply.github.com>
2025-10-23 22:45:23 +0300

    Update JavaDeseni.java

--- Task-1-HosgeldinMesajlari/JavaDeseni.java
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -4,8 +4,7 @@ public class JavaDeseni {
         System.out.println("   J A A V V A A   ");
         System.out.println("J J AAAAA V V AAAAA");
         System.out.println("   J A A V V A A   ");
-
-        
     }
 }
+
 
